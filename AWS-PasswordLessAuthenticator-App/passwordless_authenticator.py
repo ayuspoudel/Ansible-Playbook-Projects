dc1f9543d4db4370422ec447c722028dc54c318c
Ayush Poudel <ayushpoudel@usf.edu>
2024-09-07 19:24:03 -0400

    SSH into instance Successfull

--- AWS-PasswordLessAuthenticator-App/passwordless_authenticator.py
+++ AWS-PasswordLessAuthenticator-App/passwordless_authenticator.py
@@ -11,6 +11,24 @@ def ssh_into_ec2(pem_file, username, public_ip):
     except subprocess.CalledProcessError as e:
         print(f"Error connecting to EC2 instance: {e}")
 
+def run_ansible_playbook(pem_file, username, public_ip):
+    # Command to run the Ansible playbook
+    ansible_command = [
+        "ansible-playbook",
+        "playbook.yaml",
+        "-i", f"{public_ip},",  # Include comma to avoid Ansible interpreting as a file
+        "--private-key", pem_file,
+        "-u", username,
+        "--ssh-common-args", "'-o StrictHostKeyChecking=no'"
+    ]
+    
+    try:
+        # Run the Ansible playbook
+        result = subprocess.run(ansible_command, check=True)
+        print("Ansible playbook executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Ansible playbook: {e}")
+
 def main():
     pem_file = input("Enter the PEM file location: ")
     username = input("Enter the EC2 instance username: ")
@@ -18,6 +36,9 @@ def main():
 
     # SSH into the EC2 instance
     ssh_into_ec2(pem_file, username, public_ip)
+
+    # Run the Ansible playbook
+    run_ansible_playbook(pem_file, username, public_ip)
 
 if __name__ == "__main__":
     main()
