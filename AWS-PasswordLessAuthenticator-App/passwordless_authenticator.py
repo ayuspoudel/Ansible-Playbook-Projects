c82fdb182ad504e02bb70e7db7ecca2a03786b07
Ayush Poudel <ayushpoudel@usf.edu>
2024-09-07 19:18:18 -0400

    SSH into instance Successfull

--- AWS-PasswordLessAuthenticator-App/passwordless_authenticator.py
+++ AWS-PasswordLessAuthenticator-App/passwordless_authenticator.py
@@ -1,22 +1,22 @@
 import subprocess
 
-def get_user_input():
-    pem_file = input("Enter the PEM file location: ")
-    username = input("Enter the EC2 instance username: ")
-    public_ip = input("Enter the EC2 public IP: ")
-    return pem_file, username, public_ip
-
 def ssh_into_ec2(pem_file, username, public_ip):
-    ssh_command = ["ssh", "-i", pem_file, f"{username}@{public_ip}"]
+    # Add the StrictHostKeyChecking option to automatically accept the host key
+    ssh_command = ["ssh", "-o", "StrictHostKeyChecking=no", "-i", pem_file, f"{username}@{public_ip}"]
 
     try:
+        # Run the SSH command
         result = subprocess.run(ssh_command, check=True)
         print("SSH connection established successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error connecting to EC2 instance: {e}")
 
 def main():
-    pem_file, username, public_ip = get_user_input()
+    pem_file = input("Enter the PEM file location: ")
+    username = input("Enter the EC2 instance username: ")
+    public_ip = input("Enter the EC2 public IP: ")
+
+    # SSH into the EC2 instance
     ssh_into_ec2(pem_file, username, public_ip)
 
 if __name__ == "__main__":
